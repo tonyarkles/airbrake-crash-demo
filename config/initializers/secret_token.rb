855fa158497ab6d49c228c67442c96df69bda0ec
Tony Arkles <t.arkles@gmail.com>
2014-09-19 10:21:07 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirbrakeCrashDemo2::Application.config.secret_key_base = 'd171943a1b186f11219f9c4f9eb9f32174cfc62fe6aa1a1365df49525d6b692e28d235c3d26056d259652eaa77659ad248e446fedfd1e5452ff783b390c3ce94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AirbrakeCrashDemo2::Application.config.session_store :cookie_store, key: '_airbrake-crash-demo-2_session'
