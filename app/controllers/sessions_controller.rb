7f13a890360fddce8b8c6fa27915a33332da871f
Tony Arkles <t.arkles@gmail.com>
2014-09-19 10:44:10 -0600

    Super simple sign-in for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  protect_from_forgery except: :create
 
   def create
-   raise env['omniauth.auth'].to_yaml
+    user = User.from_omniauth(env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in."
   end
 end
