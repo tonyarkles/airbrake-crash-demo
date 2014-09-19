bc69d9e416aefa9eeef046998e16f774fff9a646
Tony Arkles <t.arkles@gmail.com>
2014-09-19 10:52:15 -0600

    Controller throws an exception

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 AirbrakeCrashDemo2::Application.routes.draw do
   root to: 'welcome#index'
+  get '/boom' => 'welcome#boom'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
